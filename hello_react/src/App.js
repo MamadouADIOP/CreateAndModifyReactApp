303aafcaab5f146340ebcb3c310b5a3df395cf20
Katacoda Scenario <scenario@katacoda.com>
2024-10-23 04:21:00 +0000

    update

--- hello_react/src/App.js
+++ hello_react/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Research React
         </a>
       </header>
     </div>
